4634842b7c27b4570a63fd8acbb594d5509f99b2
Danya Samoylov <130479785+Zshdun@users.noreply.github.com>
2024-01-13 07:46:29 +0700

    Add files via upload

---
+++ Services/CalcService.cs
@@ -0,0 +1,34 @@
+﻿namespace lab12.Services
+{
+	public class CalcService
+	{
+		public string calc(int firstValue, string operation, int secondValue)
+		{
+			switch (operation)
+			{
+				case "+":
+					return $"{firstValue} + {secondValue} = " + (firstValue + secondValue);
+				case "-":
+					return $"{firstValue} - {secondValue} = " + (firstValue - secondValue);
+				case "*":
+					return $"{firstValue} * {secondValue} = " + (firstValue * secondValue);
+				case "/":
+					return _division(firstValue, secondValue);
+			}
+
+			return "";
+		}
+		private string _division(int firstValue, int secondValue)
+		{
+			try
+			{
+				var divResult = firstValue / secondValue;
+				return $"{firstValue} / {secondValue} = " + (firstValue / secondValue);
+			}
+			catch (DivideByZeroException)
+			{
+				return "Can not divide by zero";
+			}
+		}
+	}
+}
